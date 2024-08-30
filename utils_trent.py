a5ce44fd8e871beacf2db3fc0113eba1429f47f6
trentrueckert <trentrueckert@gmail.com>
2024-08-30 01:04:15 -0500

    Iteration 3 - multiline byline

--- utils_trent.py
+++ utils_trent.py
@@ -100,3 +100,68 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
+''' ITERATION 3
+
+Module: Rueckert Analytics - Data Interpretations
+
+This module provides a simple, reusable foundation for my analytics projects.
+
+Process:
+In this third iteration, I declare additional variables to show skills with different data types.
+'''
+
+#####################################
+# Declare global variables - keep byline at the end
+# We will use the information in a smaller byline
+#####################################
+
+# Boolean variable to indicate if the company has international clients 
+has_international_clients: bool = True
+
+# Integer variable for the number of years in operation
+years_in_operation: int = 6
+
+# List of strings representing the skills offered by the company
+skills_offered: list = ["GitHub", "RStudio", "Python"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.7, 4.8, 4.8, 5.0, 4.9]
+
+#####################################
+# Declare a global variable named byline.
+# Make it a multiline f-string to show our information.
+#####################################
+
+byline: str = f"""
+----------------------------------------------------------
+Rueckert Analytics: Data Interpretations
+----------------------------------------------------------
+Has International Clients:  {has_international_clients}
+Years in Operation:         {years_in_operation}
+Skills Offered:             {skills_offered}
+Client Satisfaction Scores: {client_satisfaction_scores}
+"""
+
+#####################################
+# Define the get_byline() Function
+#####################################
+
+def get_byline() -> str:
+   '''Return a byline for my analytics projects.'''
+   return byline
+
+#####################################
+# Define a main() function for this module.
+#####################################
+
+def main() -> None:
+   '''Print the byline to the console when this function is called.'''
+   print(get_byline())
+
+#####################################
+# Conditional Execution
+#####################################
+
+if __name__ == '__main__':
+    main()
